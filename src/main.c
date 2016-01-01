060d3e333fe847896ebfb3dff3d6121d17d1c86c
Beeno Tung <aabbcc1241@yahoo.com.hk>
2016-01-01 15:40:55 +0800

    trying to store input grid

--- src/main.c
+++ src/main.c
@@ -1,6 +1,39 @@
 #include <stdio.h>
 
+const int _grid_size=9;
+
 int main(){
-  printf("Hello World");
+  printf("program start\n");
+
+  printf("Reading from file\n");
+  char input_filename[] = "res/input.txt";
+  /* read from file without checking */
+  char * line = NULL;
+  size_t len = 0;
+  ssize_t read;
+  FILE * input_file = fopen(input_filename, "rt");
+  char * grid_pointers[_grid_size][_grid_size];
+  int row = 0;
+  int col = 0;
+  while ((read = getline(&line, &len, input_file)) != -1 && row < _grid_size){
+//    printf("%s", line);
+    for (col=0; col<_grid_size; col++) {
+//      printf("%c",line[col]);
+      char cell = line[col];
+      grid_pointers[row][col] = &cell;
+    }
+//    printf("\nfinished row-%d\n\n",row);
+    row += 1;
+  }
+
+  printf("input grid :\n");
+  for(row=0;row<_grid_size;row++){
+    for(col=0;col<_grid_size;col++){
+      printf("%c",*grid_pointers[row][col]);
+    }
+    printf("\n");
+  }
+
+  printf("program end\n");
   return 0;
 }
