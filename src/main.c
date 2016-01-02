66d7376c86f4bb17551d64944fe38622c0da46a2
Beeno Tung <aabbcc1241@yahoo.com.hk>
2016-01-03 01:30:51 +0800

    implemented function find_possible_numbers
    to handle if there are no 'sure case'
    i.e. all cell has more than one possible numbers

--- src/main.c
+++ src/main.c
@@ -1,7 +1,58 @@
+#include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
 
 const int _grid_size=9;
+const int false=0;
+const int true=1;
+
+void print_grid(int grid[_grid_size][_grid_size]){
+  int row,col;
+  for(row=0;row<_grid_size;row++){
+    for(col=0;col<_grid_size;col++){
+      if(grid[row][col]==0)
+        printf("_");
+      else
+        printf("%d",grid[row][col]);
+    }
+    printf("\n");
+  }
+}
+
+/**
+ * @param grid : 2D array of the grid
+ * @param row,col : index of the target cell in grid
+ * @param possible_numbers : pointer to the 'output' array, to reduce memory usage
+ * @return number of possible_numbers
+ *
+ * put all number to the array, then set impossible number to zero
+*/
+int find_possible_numbers(int grid[_grid_size][_grid_size], int row, int col, int possible_numbers[_grid_size]){
+  /* return cell content directly if the cell is non-empty */
+  if(grid[row][col]!=0){
+    possible_numbers[0]=grid[row][col];
+    return 1;
+  }else{
+    int r,c;
+    int cell=grid[row][col];
+    /* init possible number */
+    for(r=0;r<_grid_size;r++)
+      possible_numbers[r]=r+1;
+    /* remove impossible number */
+    for(r=0;r<_grid_size;r++)
+        for(c=0;c<_grid_size;c++){
+          /* if same row or same col, but not same cell */
+          if(r==row ^ c==col && grid[r][c]==cell)
+            possible_numbers[grid[r][c]]=0;
+        }
+    /* move non-zero number into header and count number of possible number */
+    int count=0;
+    for(r=0;r<_grid_size;r++)
+      if(possible_numbers[r]!=0)
+        possible_numbers[count++]=possible_numbers[r];
+    return count;
+  }
+}
 
 int main(){
   printf("program start\n");
@@ -13,40 +64,53 @@ int main(){
   size_t len = 0;
   ssize_t read;
   FILE * input_file = fopen(input_filename, "rt");
-  char grid_pointers[_grid_size][_grid_size];
+  int grid[_grid_size][_grid_size];
   int row = 0;
   int col = 0;
   while ((read = getline(&line, &len, input_file)) != -1 && row < _grid_size){
     for (col=0; col<_grid_size; col++) {
       char cell = line[col];
-      grid_pointers[row][col] = cell;
+      if(cell>='1' && cell<='9'){
+        grid[row][col] = (int)(cell-'0');
+      }else if(cell=='_'){
+        grid[row][col] = 0;
+      }else{
+        printf("\nError: input is not digit or underscore");
+        return(EXIT_FAILURE);
+      }
     }
     row += 1;
   }
 
   printf("input grid :\n");
-  for(row=0;row<_grid_size;row++){
-    for(col=0;col<_grid_size;col++){
-      printf("%c",(grid_pointers)[row][col]);
-    }
-    printf("\n");
-  }
+  print_grid(grid);
 
-  printf("solving the sudoku...");
-  //TODO
-  char grid_pointers_solution[_grid_size][_grid_size];
+  printf("solving the sudoku");
+  int grid_solution[_grid_size][_grid_size];
+  int solved = false;
+  int possible_numbers[_grid_size][_grid_size][_grid_size];
+  /* init array */
+  memset(possible_numbers, 0, _grid_size*_grid_size*_grid_size*sizeof(int));
+  /* attempt to find unique possible cell, if not found, use deep first search */
+  int n_possible_numbers;
+  while (!solved){
+    printf(".");
+    for(row=0;row<_grid_size;row++)
+      for(col=0;col<_grid_size;col++){
+        n_possible_numbers = find_possible_numbers(grid, row, col, possible_numbers[row][col]);
+//        printf("\nn=%d",n_possible_numbers);
+        if(n_possible_numbers==1)
+          grid[row][col]=possible_numbers[row][col][0];
+      }
+    //TODO
+    solved = true;
+    memcpy(&grid_solution, &grid, sizeof grid);
+  }
   printf("\n");
 
   printf("solution found:\n");
-  //TODO
-  memcpy(&grid_pointers_solution, &grid_pointers, sizeof grid_pointers);
-  for(row=0;row<_grid_size;row++){
-    for(col=0;col<_grid_size;col++){
-      printf("%c",(grid_pointers_solution)[row][col]);
-    }
-    printf("\n");
-  }
+  print_grid(grid_solution);
 
   printf("program end\n");
-  return 0;
+  return(EXIT_SUCCESS);
 }
